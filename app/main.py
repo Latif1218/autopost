d86056e73c2957161dd878be78abaf4a9d0ae1d6
Latif1218 <mdsabdullotif@gmail.com>
2026-05-22 16:52:07 +0600

    update allow_origines

--- app/main.py
+++ app/main.py
@@ -61,6 +61,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://127.0.0.1:5501",
         "http://localhost:5501",
+        "https://ottomaxai.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
